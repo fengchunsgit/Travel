ce0a1c20aea70d103aa58c756c2ef1ead95204e0
fengchunsgit <1055688803@qq.com>
2018-10-09 21:14:04 +0800

    project init

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+<div>
+hello world
+</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
 }
 </script>
 
 <style>
+
 </style>
